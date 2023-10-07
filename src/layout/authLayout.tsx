fc764414261de85ff78a08d6ed70c2638e3c3501
OlacodesLA <zaccheausjide@gmail.com>
2023-10-07 20:05:55 +0100

    :iphone: Style: responsiveness fix

--- src/layout/authLayout.tsx
+++ src/layout/authLayout.tsx
@@ -10,7 +10,7 @@ const AuthLayout = ({ children }: Props) => {
   return (
     <div className="relative app-container h-screen bg-black overflow-hidden flex justify-center items-end pb-10">
       <div className="absolute inset-0 top-40 flex flex-col w-full items-center">
-        <div className="w-[400px]">{children}</div>
+        <div className="max-w-3xl mx-auto md:w-[400px] px-3">{children}</div>
       </div>
       <div className="absolute -bottom-[calc(100vh-180px)]">
         <div className="ball  bottom-0"></div>
